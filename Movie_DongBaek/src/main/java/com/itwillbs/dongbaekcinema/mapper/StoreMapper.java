53d64586837b5797e1e2061c2e7f293b5de2068c
LHJ <LHJ@DESKTOP-T1TU7A1>
2023-06-13 01:13:38 +0900

    mapUnderscoreToCamelCase=false,snack_main

---
+++ Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/mapper/StoreMapper.java
@@ -0,0 +1,16 @@
+package com.itwillbs.dongbaekcinema.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.itwillbs.dongbaekcinema.vo.SnackVO;
+
+@Mapper
+public interface StoreMapper {
+
+	List<SnackVO> selectSnackList();
+
+	
+
+}
---
+++ Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/service/StoreService.java
@@ -0,0 +1,20 @@
+package com.itwillbs.dongbaekcinema.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.dongbaekcinema.mapper.StoreMapper;
+import com.itwillbs.dongbaekcinema.vo.SnackVO;
+
+@Service
+public class StoreService {
+	@Autowired StoreMapper mapper;
+	public List<SnackVO> getSnackList() {
+		// TODO Auto-generated method stub
+		return mapper.selectSnackList();
+	}
+
+
+}
