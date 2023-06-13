e9d56ba4736b123b76099de3e8618731af4e3c62
user <user@ITWILL>
2023-06-13 17:47:48 +0900

    김경진_0613_moviecontroller / list / test / mapper 미완성

--- Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/service/MovieService.java
+++ Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/service/MovieService.java
@@ -1,5 +1,7 @@
 package com.itwillbs.dongbaekcinema.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,27 @@ public class MovieService {
 	public int registMovie(MovieVO movie) {
 		return mapper.insertMovie(movie);
 	}
+	
+
+	// 영화 정보 조회 요청을 위한 getMovieList() 메서드 정의
+	public MovieVO getMovie(int movie_num) {
+		System.out.println("ReservationService - getMovie()");
+		
+		return mapper.selectMovie(movie_num);
+	}
+	
+	
+	// 영화 목록 조회 요청을 위한 getMovieList() 메서드 정의 - 현재상영작? select_presentMovie
+	public List<MovieVO> getMovieList() {
+		System.out.println("ReservationService - getMovieList()");
+				
+		return mapper.select_presentMovie();
+	}
+	
+	//영화목록조회  - 상영예정작? select_prepareMovie
+	
+	
+	//영화목록조회  - 현재상영작? select_presentMovie
+	//영화목록조회  - 상영예정작? select_prepareMovie
 
 }
