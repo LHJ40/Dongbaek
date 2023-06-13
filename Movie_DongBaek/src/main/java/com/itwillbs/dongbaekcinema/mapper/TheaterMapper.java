bc8bb861e6db2cb445efc9c737ce7eeab9684a2f
user <user@ITWILL>
2023-06-13 17:38:30 +0900

    home

--- Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/mapper/TheaterMapper.java
+++ Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/mapper/TheaterMapper.java
@@ -3,7 +3,9 @@ package com.itwillbs.dongbaekcinema.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import com.itwillbs.dongbaekcinema.vo.ScheduleVO;
 import com.itwillbs.dongbaekcinema.vo.TheaterVO;
 
 @Mapper
@@ -12,5 +14,9 @@ public interface TheaterMapper {
 	List<TheaterVO> selectTheaterList();
 
 	TheaterVO selectTheater(int theater_num);
+
+	List<ScheduleVO> selectSchedule(@Param("theater_num") int theater_num,@Param("play_date") String play_date);
+
+
 
 }
