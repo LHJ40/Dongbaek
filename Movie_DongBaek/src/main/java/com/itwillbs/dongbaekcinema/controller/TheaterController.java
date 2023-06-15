dc7ddd310e40f97f79e40f3064544d844fbd457f
itwillbs51 <itwillbs51@gmail.com>
2023-06-15 20:55:12 +0900

    Revert "home"
    This reverts commit bc8bb861e6db2cb445efc9c737ce7eeab9684a2f.

--- Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/controller/TheaterController.java
+++ Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/controller/TheaterController.java
@@ -3,20 +3,15 @@ package com.itwillbs.dongbaekcinema.controller;
 import java.time.LocalDate;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itwillbs.dongbaekcinema.service.TheaterService;
-import com.itwillbs.dongbaekcinema.vo.PlayVO;
-import com.itwillbs.dongbaekcinema.vo.ScheduleVO;
 import com.itwillbs.dongbaekcinema.vo.SnackVO;
 import com.itwillbs.dongbaekcinema.vo.TheaterVO;
 
@@ -45,17 +40,6 @@ public class TheaterController {
 		model.addAttribute("theater", theater);		
 		System.out.println(theater);
 		return theater;
-	}
-	
-	@ResponseBody
-	@GetMapping("getSchedule")
-	public List<ScheduleVO> getSchedule(int theater_num, String play_date, Model model) {
-		System.out.println("getSchedule");
-		List<ScheduleVO> schedule = service.getSchedule(theater_num,play_date);
-		model.addAttribute("schedule", schedule);
-		System.out.println(schedule);
-		
-		return schedule;
 	}
 	@GetMapping("theater-price_tap")
 	public String theater_price_tap() {
--- Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/service/TheaterService.java
+++ Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/service/TheaterService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.itwillbs.dongbaekcinema.mapper.TheaterMapper;
-import com.itwillbs.dongbaekcinema.vo.ScheduleVO;
 import com.itwillbs.dongbaekcinema.vo.TheaterVO;
 
 @Service
@@ -21,10 +20,5 @@ public class TheaterService {
 		// TODO Auto-generated method stub
 		return mapper.selectTheater(theater_num);
 	}
-	public List<ScheduleVO> getSchedule(int theater_num, String play_date) {
-		// TODO Auto-generated method stub
-		return mapper.selectSchedule(theater_num,play_date);
-	}
-	
 
 }
