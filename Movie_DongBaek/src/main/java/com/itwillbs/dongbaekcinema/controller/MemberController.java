ea9a6f1bf7664ad06719ab77deb55636460bb42a
sonJY <A@222.119.133.232>
2023-06-25 22:58:40 +0900

    손지영 - 230625 - 찜하기 구현 중(main 성공)

--- Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/controller/MemberController.java
+++ Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/controller/MemberController.java
@@ -386,7 +386,7 @@ public class MemberController {
 		System.out.println(insertCount);
 		// 비회원 로그인(가입) 성공 시 success_back으로 이동
 		if(insertCount > 0) {
-			session.setAttribute("member_id", noMember.getMember_name()); // name으로 불리게 하기
+			session.setAttribute("member_id", noMember.getMember_id());
 			session.setAttribute("member_type", "비회원");
 			
 			// 나중에 작업하던 곳으로 돌아가게 설정하기(예매-좌석)
