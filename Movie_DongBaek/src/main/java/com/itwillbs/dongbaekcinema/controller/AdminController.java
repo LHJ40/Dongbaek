2dab9f877e75164232416cc6134575e49baacfd0
clearxxx <gnoej22@gmail.com>
2023-06-26 21:14:53 +0900

    이수정 - 230626 관리자 메인, 상영스케줄, CS 페이지 접근 금지 설정

--- Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/controller/AdminController.java
+++ Movie_DongBaek/src/main/java/com/itwillbs/dongbaekcinema/controller/AdminController.java
@@ -1212,26 +1212,18 @@ public class AdminController {
 		
 		try {
 			// ------------------------------------------------------------------------------
-			// 업로드 디렉토리를 날짜별 디렉토리로 자동 분류하기
-			// => 하나의 디렉토리에 너무 많은 파일이 존재하면 로딩 시간 길어지며 관리도 불편
-			// => 따라서, 날짜별 디렉토리 구별 위해 java.util.Date 클래스 활용
-			// 1. Date 객체 생성(기본 생성자 호출하여 시스템 날짜 정보 활용)
 			Date date = new Date(); // Mon Jun 19 11:26:52 KST 2023
 //		System.out.println(date);
-			// 2. SimpleDateFormat 클래스를 활용하여 날짜 형식을 "yyyy/MM/dd" 로 지정
 			// => 디렉토리 구조로 바로 활용하기 위해 날짜 구분 기호를 슬래시(/)로 지정
-			// => 디렉토리 구분자를 가장 정확히 표현하려면 File.pathSeperator 또는 File.seperator 상수 활용
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 			// 3. 기존 업로드 경로에 날짜 경로 결합하여 저장
 			subDir = sdf.format(date);
 			saveDir += "/" + subDir;
 			// --------------------------------------------------------------
 			// java.nio.file.Paths 클래스의 get() 메서드를 호출하여
-			// 실제 경로를 관리하는 java.nio.file.Path 타입 객체 리턴받기
 			// => 파라미터 : 실제 업로드 경로
 			Path path = Paths.get(saveDir);
 			
-			// Files 클래스의 createDirectories() 메서드를 호출하여
 			// Path 객체가 관리하는 경로 생성(존재하지 않으면 거쳐가는 경로들 중 없는 경로 모두 생성)
 			Files.createDirectories(path);
 		} catch (IOException e) {
@@ -1242,18 +1234,11 @@ public class AdminController {
 		MultipartFile mFile1 = faqInfo.getFile1();
 //		System.out.println("원본파일명1 : " + mFile1.getOriginalFilename());
 		
-		// 파일명 중복 방지를 위한 대첵
-		// 현재 시스템(서버)에서 랜덤ID 값을 추출하여 파일명 앞에 붙여서
 		// "랜덤ID값_파일명.확장자" 형식으로 중복 파일명 처리
-		// => 랜덤ID 생성은 java.util.UUID 클래스 활용(UUID = 범용 고유 식별자)
 		String uuid = UUID.randomUUID().toString();
 //		System.out.println("uuid : " + uuid);
 		
-		// 생성된 UUID 값을 원본 파일명 앞에 결합(파일명과 구분을 위해 _ 기호 추가)
-		// => 나중에 사용자 다운로드 시 원본 파일명 표시를 위해 분리할 때 구분자로 사용
-		//    (가장 먼저 만나는 _ 기호를 기준으로 문자열 분리하여 처리)
-		// 생성된 UUID 값(8자리 추출)과 업로드 파일명을 결합하여 CsInfoVO 객체에 저장(구분자로 _ 기호 추가)
-		// => 단, 파일명이 존재하는 경우에만 파일명 생성(없을 경우를 대비하여 기본 파일명 널스트링으로 처리)
+		//  파일명이 존재하는 경우에만 파일명 생성(없을 경우를 대비하여 기본 파일명 널스트링으로 처리)
 		faqInfo.setCs_file("");
 		
 		// 파일명을 저장할 변수 선언
@@ -1274,16 +1259,8 @@ public class AdminController {
 		
 		
 		// 게시물 등록 작업 요청 결과 판별
-		// => 성공 시 업로드 파일을 실제 디렉토리에 이동시킨 후 BoardList 서블릿 리다이렉트
-		// => 실패 시 "글 쓰기 실패!" 메세지 출력 후 이전페이지 돌아가기 처리
 		if(insertCount > 0) { // 성공
 			try {
-				// 업로드 된 파일은 MultipartFile 객체에 의해 임시 디렉토리에 저장되어 있으며
-				// 글쓰기 작업 성공 시 임시 디렉토리 -> 실제 디렉토리로 이동 작업 필요
-				// MultipartFile 객체의 transferTo() 메서드를 호출하여 실제 위치로 이동(업로드)
-				// => 비어있는 파일은 이동할 수 없으므로(= 예외 발생) 제외
-				// => File 객체 생성 시 지정한 디렉토리에 지정한 이름으로 파일이 이동(생성)됨
-				//    따라서, 이동할 위치의 파일명도 UUID 가 결합된 파일명을 지정해야한다!
 				if(!mFile1.getOriginalFilename().equals("")) {
 					mFile1.transferTo(new File(saveDir, fileName1));
 				}
@@ -1356,82 +1333,39 @@ public class AdminController {
         }		
 		
 
-//
-//		System.out.println("faq_modify_pro csTypeNo :" + csTypeNo + ", faqInfo" + faqInfo + ", files" + files);
-//		// 공지사항 글정보 변경
-//		int updateCount = admin_service.updateCs(csTypeNo, faqInfo, files);
-//		
-//		
-//		if(updateCount > 0 ) { // 답변 등록 성공 시
-//			// 페이지 정보 저장
-//			model.addAttribute("pageNo", pageNo);
-//			// 자주묻는 질문 게시판으로 이동
-//			return "redirect:/admin_cs_faq";
-//		} else {
-//			System.out.println("Faq - update 실패!");
-//			model.addAttribute("msg", "답변 등록이 실패하였습니다!");
-//			return "fail_back"; // 실패 시 이동할 페이지
-//		}
 		
-		
-		// 이클립스 프로젝트 상에 업로드 폴더(upload) 생성 필요 
-		// => 주의! 외부에서 접근하도록 하려면 resources 폴더 내에 upload 폴더 생성
-		// 이클립스가 관리하는 프로젝스 상의 가상 업로드 경로에 대한 실제 업로드 경로 알아내기
-		// => request.getRealPath() 대신 request.getServletContext.getRealPath() 메서드 또는
-		//    세션 객체를 활용한 session.getServletContext().getRealPath() 메서드 사용
 //		System.out.println(request.getRealPath("/resources/upload")); // Deprecated 처리된 메서드
 		String uploadDir = "/resources/upload"; 
-//		String saveDir = request.getServletContext().getRealPath(uploadDir); // 사용 가능
 		String saveDir = session.getServletContext().getRealPath(uploadDir);
 //		System.out.println("실제 업로드 경로 : "+ saveDir);
-		// 실제 업로드 경로 : D:\Shared\Spring\workspace_spring5\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\Spring_MVC_Board\resources\ upload
 		
 		String subDir = ""; // 서브디렉토리(날짜 구분)
 		
 		try {
 			// ------------------------------------------------------------------------------
-			// 업로드 디렉토리를 날짜별 디렉토리로 자동 분류하기
-			// => 하나의 디렉토리에 너무 많은 파일이 존재하면 로딩 시간 길어지며 관리도 불편
-			// => 따라서, 날짜별 디렉토리 구별 위해 java.util.Date 클래스 활용
-			// 1. Date 객체 생성(기본 생성자 호출하여 시스템 날짜 정보 활용)
 			Date date = new Date(); // Mon Jun 19 11:26:52 KST 2023
 //		System.out.println(date);
-			// 2. SimpleDateFormat 클래스를 활용하여 날짜 형식을 "yyyy/MM/dd" 로 지정
-			// => 디렉토리 구조로 바로 활용하기 위해 날짜 구분 기호를 슬래시(/)로 지정
-			// => 디렉토리 구분자를 가장 정확히 표현하려면 File.pathSeperator 또는 File.seperator 상수 활용
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 			// 3. 기존 업로드 경로에 날짜 경로 결합하여 저장
 			subDir = sdf.format(date);
 			saveDir += "/" + subDir;
 			// --------------------------------------------------------------
-			// java.nio.file.Paths 클래스의 get() 메서드를 호출하여
-			// 실제 경로를 관리하는 java.nio.file.Path 타입 객체 리턴받기
 			// => 파라미터 : 실제 업로드 경로
 			Path path = Paths.get(saveDir);
 			
-			// Files 클래스의 createDirectories() 메서드를 호출하여
 			// Path 객체가 관리하는 경로 생성(존재하지 않으면 거쳐가는 경로들 중 없는 경로 모두 생성)
 			Files.createDirectories(path);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		// BoardVO 객체에 전달된 MultipartFile 객체 꺼내기
 		MultipartFile mFile1 = faqInfo.getFile1();
 //		System.out.println("원본파일명1 : " + mFile1.getOriginalFilename());
 		
-		// 파일명 중복 방지를 위한 대첵
-		// 현재 시스템(서버)에서 랜덤ID 값을 추출하여 파일명 앞에 붙여서
 		// "랜덤ID값_파일명.확장자" 형식으로 중복 파일명 처리
-		// => 랜덤ID 생성은 java.util.UUID 클래스 활용(UUID = 범용 고유 식별자)
 		String uuid = UUID.randomUUID().toString();
 //		System.out.println("uuid : " + uuid);
 		
-		// 생성된 UUID 값을 원본 파일명 앞에 결합(파일명과 구분을 위해 _ 기호 추가)
-		// => 나중에 사용자 다운로드 시 원본 파일명 표시를 위해 분리할 때 구분자로 사용
-		//    (가장 먼저 만나는 _ 기호를 기준으로 문자열 분리하여 처리)
-		// 생성된 UUID 값(8자리 추출)과 업로드 파일명을 결합하여 CsInfoVO 객체에 저장(구분자로 _ 기호 추가)
-		// => 단, 파일명이 존재하는 경우에만 파일명 생성(없을 경우를 대비하여 기본 파일명 널스트링으로 처리)
 		faqInfo.setCs_file("");
 		
 		// 파일명을 저장할 변수 선언
@@ -1446,22 +1380,13 @@ public class AdminController {
 //		System.out.println("실제 업로드 파일명1 : " + faqInfo.getCs_file());
 		
 		// -----------------------------------------------------------------------------------
-		// BoardService - registBoard() 메서드를 호출하여 게시물 등록 작업 요청
-		// => 파라미터 : BoardVO 객체    리턴타입 : int(insertCount)
+		// => 파라미터 : csTypeNo, faqInfo 객체    리턴타입 : int(updateCount)
 		int updateCount = admin_service.updateCs(csTypeNo, faqInfo);
 		
 		
 		// 게시물 등록 작업 요청 결과 판별
-		// => 성공 시 업로드 파일을 실제 디렉토리에 이동시킨 후 BoardList 서블릿 리다이렉트
-		// => 실패 시 "글 쓰기 실패!" 메세지 출력 후 이전페이지 돌아가기 처리
 		if(updateCount > 0) { // 성공
 			try {
-				// 업로드 된 파일은 MultipartFile 객체에 의해 임시 디렉토리에 저장되어 있으며
-				// 글쓰기 작업 성공 시 임시 디렉토리 -> 실제 디렉토리로 이동 작업 필요
-				// MultipartFile 객체의 transferTo() 메서드를 호출하여 실제 위치로 이동(업로드)
-				// => 비어있는 파일은 이동할 수 없으므로(= 예외 발생) 제외
-				// => File 객체 생성 시 지정한 디렉토리에 지정한 이름으로 파일이 이동(생성)됨
-				//    따라서, 이동할 위치의 파일명도 UUID 가 결합된 파일명을 지정해야한다!
 				if(!mFile1.getOriginalFilename().equals("")) {
 					mFile1.transferTo(new File(saveDir, fileName1));
 				}
